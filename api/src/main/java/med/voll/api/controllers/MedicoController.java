7664ad34601c2ce541c3880c866ebd81616a5883
Pedro Lucas <pedro.lucas@guaraves.local>
2024-04-26 16:15:23 -0300

    FEAT: get request, implementation

--- api/src/main/java/med/voll/api/controllers/MedicoController.java
+++ api/src/main/java/med/voll/api/controllers/MedicoController.java
@@ -3,22 +3,28 @@ package med.voll.api.controllers;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import med.voll.api.models.medico.DadosCadastroMedico;
+import med.voll.api.models.medico.DadosListagemMedico;
 import med.voll.api.models.medico.Medico;
 import med.voll.api.repositories.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("medicos")
 public class MedicoController {
     @Autowired
     private MedicoRepository medicoRepository;
+
     @PostMapping
     @Transactional
     public void postMedico(@RequestBody @Valid DadosCadastroMedico dadosCadastroMedico){
         medicoRepository.save(new Medico(dadosCadastroMedico));
+    }
+
+    @GetMapping
+    public List<DadosListagemMedico> getMedicos() {
+        return medicoRepository.findAll().stream().map(DadosListagemMedico::new).toList();
     }
 }
