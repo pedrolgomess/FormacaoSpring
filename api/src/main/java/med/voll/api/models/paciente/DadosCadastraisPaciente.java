55c8632b309eb1a997102b3e5a7b8512b946fd2f
Pedro Lucas <pedro.lucas@guaraves.local>
2024-04-26 14:01:59 -0300

    FEAT: Bean validation, DadosCadastraisPaciente

--- api/src/main/java/med/voll/api/models/paciente/DadosCadastraisPaciente.java
+++ api/src/main/java/med/voll/api/models/paciente/DadosCadastraisPaciente.java
@@ -1,6 +1,23 @@
 package med.voll.api.models.paciente;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import med.voll.api.models.endereco.DadosEndereco;
 
-public record DadosCadastraisPaciente(String nome, String email, String telefone, String cpf, DadosEndereco dadosEndereco) {
+public record DadosCadastraisPaciente(
+        @NotBlank
+        String nome,
+        @NotBlank
+        @Email
+        String email,
+        @NotBlank
+        String telefone,
+        @NotBlank
+        @Pattern(regexp = "\\d{15}")
+        String cpf,
+        @NotBlank
+        @Valid
+        DadosEndereco dadosEndereco) {
 }
