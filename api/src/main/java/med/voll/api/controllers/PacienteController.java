5dd6cf1393ed0a367b5ffee8bbb9102089b56d96
Pedro Lucas <pedro.lucas@guaraves.local>
2024-04-26 11:38:47 -0300

    FIX: Ajustado o tamanho da coluna do cpf para 15, estava 6

--- api/src/main/java/med/voll/api/controllers/PacienteController.java
+++ api/src/main/java/med/voll/api/controllers/PacienteController.java
@@ -1,5 +1,6 @@
 package med.voll.api.controllers;
 
+import jakarta.transaction.Transactional;
 import med.voll.api.models.paciente.DadosCadastraisPaciente;
 import med.voll.api.models.paciente.Paciente;
 import med.voll.api.repositories.PacienteRepository;
@@ -15,6 +16,7 @@ public class PacienteController {
     @Autowired
     private PacienteRepository pacienteRepository;
     @PostMapping
+    @Transactional
     public void postPaciente(@RequestBody DadosCadastraisPaciente dadosCadastraisPaciente){
         pacienteRepository.save(new Paciente(dadosCadastraisPaciente));
     }
---
+++ api/src/main/resources/db/migration/V2__create-table-pacientes.sql
@@ -0,0 +1,13 @@
+create table pacientes(
+    id  serial primary key,
+    nome varchar(100) not null,
+    telefone varchar(100) not null unique,
+    cpf varchar(6) not null unique,
+    logradouro varchar(100) not null,
+    bairro varchar(100) not null,
+    cep varchar(9) not null,
+    complemento varchar(100),
+    numero varchar(20),
+    uf char(2) not null,
+    cidade varchar(100) not null
+);
