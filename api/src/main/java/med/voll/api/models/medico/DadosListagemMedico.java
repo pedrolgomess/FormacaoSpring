c15ad562c11af05dca73e58161bf16cd2f385491
Pedro Lucas <pedro.lucas@guaraves.local>
2024-04-30 16:37:35 -0300

    FEAT: Id no objeto do paciente

--- api/src/main/java/med/voll/api/models/medico/DadosListagemMedico.java
+++ api/src/main/java/med/voll/api/models/medico/DadosListagemMedico.java
@@ -2,10 +2,10 @@ package med.voll.api.models.medico;
 
 import med.voll.api.models.enums.Especialidade;
 
-public record DadosListagemMedico(String nome, String email, String crm, Especialidade especialidade) {
+public record DadosListagemMedico(Long id, String nome, String email, String crm, Especialidade especialidade) {
 
     public DadosListagemMedico(Medico medico) {
-        this(medico.getNome(), medico.getEmail(), medico.getCrm(), medico.getEspecialidade());
+        this(medico.getId(), medico.getNome(), medico.getEmail(), medico.getCrm(), medico.getEspecialidade());
     }
 
 }
