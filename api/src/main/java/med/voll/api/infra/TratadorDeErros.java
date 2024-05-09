0270b17b5d6dbc7f9b81c40cae1a08bbc7f63f50
Pedro Lucas <pedro.lucas@guaraves.local>
2024-05-09 09:46:16 -0300

    Tratador de erros, realizado

---
+++ api/src/main/java/med/voll/api/infra/TratadorDeErros.java
@@ -0,0 +1,29 @@
+package med.voll.api.infra;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class TratadorDeErros {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity erro404(){
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity erro400(MethodArgumentNotValidException methodArgumentNotValidException){
+        var erros = methodArgumentNotValidException.getFieldErrors();
+        return ResponseEntity.badRequest().body(erros.stream().map(DadosErrosValidacao::new).toList());
+    }
+
+    private record DadosErrosValidacao(String campo, String mensagem){
+        public DadosErrosValidacao(FieldError fieldError){
+            this(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+    }
+}
