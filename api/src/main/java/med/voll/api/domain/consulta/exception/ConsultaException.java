a7a21d125836b140cb89b1bf43dcf61c4e1f747a
Pedro Lucas <pedro.lucas@guaraves.local>
2024-05-15 10:20:19 -0300

    FEAT: Validações do médico

---
+++ api/src/main/java/med/voll/api/domain/consulta/exception/ConsultaException.java
@@ -0,0 +1,7 @@
+package med.voll.api.domain.consulta.exception;
+
+public class ConsultaException extends RuntimeException {
+    public ConsultaException(String mensagem) {
+        super(mensagem);
+    }
+}
--- api/src/main/java/med/voll/api/services/AgendaDeConsulta.java
+++ api/src/main/java/med/voll/api/services/AgendaDeConsulta.java
@@ -2,6 +2,8 @@ package med.voll.api.services;
 
 import med.voll.api.domain.consulta.Consulta;
 import med.voll.api.domain.consulta.DadosConsulta;
+import med.voll.api.domain.consulta.exception.ConsultaException;
+import med.voll.api.domain.medico.Medico;
 import med.voll.api.repositories.ConsultaRepository;
 import med.voll.api.repositories.MedicoRepository;
 import med.voll.api.repositories.PacienteRepository;
@@ -18,10 +20,21 @@ public class AgendaDeConsulta {
     private PacienteRepository pacienteRepository;
 
     public void agendar(DadosConsulta dadosConsulta){
-        var medico = medicoRepository.findById(dadosConsulta.medicoId()).get();
+        if (dadosConsulta.medicoId() != null && !medicoRepository.existsById(dadosConsulta.medicoId())){
+            throw new ConsultaException("Id do médico não existe");
+        }
+        if (!pacienteRepository.existsById(dadosConsulta.pacienteId())){
+            throw new ConsultaException("Id do paciente não existe");
+        }
+
+        var medico = escolherMedico(dadosConsulta);
         var paciente = pacienteRepository.findById(dadosConsulta.pacienteId()).get();
 
         var consulta = new Consulta(null, medico,paciente, dadosConsulta.timestamp());
         consultaRepository.save(consulta);
+    }
+
+    private Medico escolherMedico(DadosConsulta dadosConsulta){
+        return null;
     }
 }
